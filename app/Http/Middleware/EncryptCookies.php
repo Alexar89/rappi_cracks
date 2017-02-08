61d9ef3aab340c31488d6d24ebd450c01a9a2fd9
Diego DM. Mora <diegomora@italcoldg.com>
2017-02-08 17:08:04 -0500

    Commit inicial del proyecto Rappi

---
+++ app/Http/Middleware/EncryptCookies.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace rappi_cracks\Http\Middleware;
+
+use Illuminate\Cookie\Middleware\EncryptCookies as BaseEncrypter;
+
+class EncryptCookies extends BaseEncrypter
+{
+    /**
+     * The names of the cookies that should not be encrypted.
+     *
+     * @var array
+     */
+    protected $except = [
+        //
+    ];
+}
